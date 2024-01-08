4fcacebc26230622d0ccc9f79891d62611abc78e
Grant Kerr <grant.kerr@airapps.com>
2024-01-08 16:06:19 +0000

    Fix ruby error

--- Fastlane/testing_lanes.rb
+++ Fastlane/testing_lanes.rb
@@ -84,7 +84,7 @@ lane :test_project do |options|
       package_path: options[:package_path], # Optional path to the SPM package to test.
       build_for_testing: options.fetch(:build_for_testing, nil),
       test_without_building: options.fetch(:test_without_building, nil),
-      disable_package_automatic_updates: false # true, # Makes xcodebuild -showBuildSettings more reliable too.
+      disable_package_automatic_updates: false, # true, # Makes xcodebuild -showBuildSettings more reliable too.
       skip_package_dependencies_resolution: options.fetch(:disable_automatic_package_resolution, false)
     )
   rescue StandardError => e
